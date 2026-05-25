29f65f09c8046266c326db51fb40598aa4db8917
Faith Ngwata <faithngwata059@gmail.com>
2026-05-25 16:32:46 +0530

    Plus a shouter function

---
+++ 04_functions/shouter.py
@@ -0,0 +1,9 @@
+# 1. Define the function
+def shout(text):
+    loud_text = text.upper() + "!!!"
+    print(loud_text)
+
+# 2. Call the function
+shout("hello")
+shout("watch out")
+shout("python is fun")
