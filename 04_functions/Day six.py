d9a99799480321bb28fdb46d56d0d20665def1aa
Faith Ngwata <faithngwata059@gmail.com>
2026-05-26 10:53:29 +0530

    Added a currency converter function to the codebase.

--- 04_functions/Day six.py
+++ 04_functions/Day six.py
@@ -12,3 +12,16 @@ create_welcome_message(user_name_input, user_city_input)
 2. Take the user inout
 3. Call the function using the user input data
 """
+
+#Currency converter
+def convert_usd_to_inr(usd_amount):
+    # Let's assume 1 USD = 95.41 INR
+    exchange_rate = 95.41
+    inr_result = usd_amount * exchange_rate
+    return inr_result
+
+# 2. Get input from the user
+raw_input = input("Enter the amount in USD ($): ")
+clean_usd = float(raw_input)
+final_inr = convert_usd_to_inr(clean_usd)
+print("Total value: ₹" + str(final_inr))
