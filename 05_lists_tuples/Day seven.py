8b1e1f9c94e4179ba2eff44092b4034ea0bcac90
Faith Ngwata <faithngwata059@gmail.com>
2026-05-27 10:45:46 +0530

    Added a grocery list that takes user input and adds it to the list. Also added a function that prints the list in a nice format.

--- 05_lists_tuples/Day seven.py
+++ 05_lists_tuples/Day seven.py
@@ -8,3 +8,22 @@ video_games[1] = "Metroid"
 # 4. Adding a new item to the end of the list
 video_games.append("Halo")
 print(video_games)
+
+#Grocery list program that accepts user input
+# 1. Start with an empty list
+grocery_list = []
+print("--- INTERACTIVE GROCERY LIST ---")
+print("Type your items one by one. When you are finished, type 'stop'.")
+print("--------------------------------")
+# 2. Use a while loop to keep asking for items
+while True:
+    user_item = input("Enter an item: ")
+    if user_item.lower() == "stop":
+        break
+    else:
+        grocery_list.append(user_item)
+        print("Added: " + user_item)
+# 3. Once the loop breaks, show the final list
+print("\n--- YOUR FINAL GROCERY LIST ---")
+for item in grocery_list:
+    print("- " + item)
