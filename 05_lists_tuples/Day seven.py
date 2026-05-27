4ae9fe98a405d6026c5fe33431c2568d32ecc616
Faith Ngwata <faithngwata059@gmail.com>
2026-05-27 10:41:58 +0530

    Added day seven file

---
+++ 05_lists_tuples/Day seven.py
@@ -0,0 +1,10 @@
+# 1. Create a list of video games
+video_games = ["Minecraft", "Zelda", "Cyberpunk", "Mario"]
+# 2. Accessing items using their position
+print("The first game is: " + video_games[0])
+print("The third game is: " + video_games[2])
+# 3. Changing an item in the list
+video_games[1] = "Metroid"
+# 4. Adding a new item to the end of the list
+video_games.append("Halo")
+print(video_games)
