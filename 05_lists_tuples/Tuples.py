d42bdde4250b1a22cd0e9e1a883fccff3547dd97
Faith Ngwata <faithngwata059@gmail.com>
2026-05-28 20:45:54 +0530

    Added more examples

--- 05_lists_tuples/Tuples.py
+++ 05_lists_tuples/Tuples.py
@@ -1,2 +1,11 @@
 home_location = (40.7128, -74.0060)
 print("Latitude: " + str(home_location[0]))
+
+fruits = ("apple", "banana", "mango")
+print(fruits)
+print(fruits[0])
+
+student = ("Faith", 20, "Computer Science Engineering")
+print("Name:", student[0])
+print("Age:", student[1])
+print("Course:", student[2])
