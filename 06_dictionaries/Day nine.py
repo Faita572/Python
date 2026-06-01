119b377e2bc214ca65ddeb62843175f77fbc51bd
Faith Ngwata <faithngwata059@gmail.com>
2026-06-01 19:48:40 +0530

    put an example

--- 06_dictionaries/Day nine.py
+++ 06_dictionaries/Day nine.py
@@ -1,3 +1,20 @@
 game_inventory = {"swords": 3, "potions": 5, "shields": 1}
 print(game_inventory.get("potions", 0)) # Prints: 5
 print(game_inventory.get("diamonds", 0)) 
+
+
+#looping through dictionaries
+movie_ratings = {
+    "Micheal": 9.0,
+    "Thunderbolts": 7.7,
+    "Hokum": 8.6
+}
+print("--- MOVIE NAMES ---")
+for movie in movie_ratings.keys():
+    print(movie)
+print("\n--- RATINGS ---")
+for rating in movie_ratings.values():
+    print(rating)
+print("\n--- FULL REVIEW ---")
+for movie, rating in movie_ratings.items():
+    print(movie + " has a rating of " + str(rating))
