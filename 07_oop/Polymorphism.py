9b4bef46063eded40374e13c559df118eec843cf
Faith Ngwata <faithngwata059@gmail.com>
2026-06-09 11:04:19 +0530

    Added polymorphism

---
+++ 07_oop/Polymorphism.py
@@ -0,0 +1,15 @@
+class Warrior:
+    def use_ability(self):
+        print("The Warrior uses: Blade Storm!")
+
+class Medic:
+    def use_ability(self):
+        print("The Medic uses: Health Drone!")
+
+# We put two completely different types of objects into a single list
+party_members = [Warrior(), Medic()]
+
+# We loop through them and call the EXACT same function name
+for member in party_members:
+    member.use_ability() 
+ # Python is smart enough to run the correct version for each object!
