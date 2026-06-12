31047201cb7d6ecd5d49b06f9689dcea4cc2dbf0
Faith Ngwata <faithngwata059@gmail.com>
2026-06-12 21:10:27 +0530

    Scenario notes

---
+++ 07_oop/OOP_combined_2.py
@@ -0,0 +1,4 @@
+#A streaming service
+#You shouldn't be able to change your subscription status or password without passing safety checks
+# Encapsulation makes sure that your data is hidden using double underscores __.
+
