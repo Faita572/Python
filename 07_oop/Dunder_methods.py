b8585766a8dcb7515a40867003d03938d06ef05e
Faith Ngwata <faithngwata059@gmail.com>
2026-06-13 21:38:06 +0530

    Added the data

--- 07_oop/Dunder_methods.py
+++ 07_oop/Dunder_methods.py
@@ -4,3 +4,15 @@
 # __add__ for addition, etc
 #You can even create your own Dunder methods to customize behavior
 
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    # The __str__ magic method overrides what happens when you print the object
+    def __str__(self):
+        return f"'{self.title}' written by {self.author}"
+
+my_book = Book("King of Wrath", "Ana Huang")
+
+print(my_book)
