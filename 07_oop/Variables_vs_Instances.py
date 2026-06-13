309e729473f140cc95e89da74c250d9b58f3e044
Faith Ngwata <faithngwata059@gmail.com>
2026-06-13 21:28:11 +0530

    Introced this concept of class variables and instance variables in python

---
+++ 07_oop/Variables_vs_Instances.py
@@ -0,0 +1,5 @@
+#Every time you create an account or a character its data belongs strictly to that individual object
+#Those are called Instance Variables (defined using self)
+#But what if you want a variable that is shared across every single object built from that blueprint?
+#For example tracking the total number of users registered on your streaming service?
+#For that we use a Class Variable
