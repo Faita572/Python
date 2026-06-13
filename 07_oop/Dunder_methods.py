66a0b24e7c99a6a56c554199c525ec3cf42956c5
Faith Ngwata <faithngwata059@gmail.com>
2026-06-13 21:36:26 +0530

    Added pre-requistist for the dunder methods

---
+++ 07_oop/Dunder_methods.py
@@ -0,0 +1,6 @@
+#Python uses special methods wrapped in double underscores (called "Dunder" methods or Magic Methods) to control how objects behave
+#There is __init__ for object initialization, 
+#__str__ for string representation, 
+# __add__ for addition, etc
+#You can even create your own Dunder methods to customize behavior
+
